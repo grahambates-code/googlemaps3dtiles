53385d2b6e5037406fde9025955033eaa3e1118f
grahambates-code <mog.mog@gmail.com>
2024-11-21 00:41:57 +0000

    add gitpod

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { ghPages } from 'vite-plugin-gh-pages';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), ghPages()],
+  build: {
+    minify: false, // Disable minification for easier debugging
+  },
+  server: {
+    host: true, // Allows access from external networks
+    port: 5173,
+    https: false, // Optional: Change to true if needed
+  },
+  base: '/googlemaps3dtiles/', // Replace with your repository name
+});
